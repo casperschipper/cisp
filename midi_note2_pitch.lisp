41041c8b2f9999bc21924989259153f1b149b378
Casper Schipper <casper.schipper@gmail.com>
2022-07-20 21:22:53 +0200

    Midi

---
+++ midi_note2_pitch.lisp
@@ -0,0 +1,6 @@
+(~ pitch
+	(hold (reset 
+		(walk 60 (seq 2 5 7))
+		(st 63)
+		(seq 4 5)) 
+	(hold (seq 1 1 1 1 1 3 2) (seq 1 2))))x
---
+++ midi_note_3.lisp
@@ -0,0 +1,11 @@
+(midi-note-channel-sync
+	(~ pitch)
+	(~ dura)
+	(~ velo)
+	(st 1))
+
+(~ pitch (seq 60 64 67))
+
+(~ dura (st 0.125))
+
+(~ velo (st 100))
